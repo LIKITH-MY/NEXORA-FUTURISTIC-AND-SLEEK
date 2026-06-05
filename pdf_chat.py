7785787789e63732606c28b07161a65a588700e2
LIKITH-MY <likitharadhyaa48@gmail.com>
2026-06-05 11:24:52 +0530

    Update pdf_chat.py

--- pdf_chat.py
+++ pdf_chat.py
@@ -1,1 +1,19 @@
 # PDF extraction logic
+from PyPDF2 import PdfReader
+
+def extract_text_from_pdf(uploaded_file):
+    try:
+        reader = PdfReader(uploaded_file)
+
+        pages = []
+
+        for page in reader.pages:
+            text = page.extract_text()
+
+            if text:
+                pages.append(text)
+
+        return "\n".join(pages)
+
+    except:
+        return ""
